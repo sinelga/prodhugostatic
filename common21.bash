bb1a952bb9a45afc18a24223c08307aad515b8e6
juno <juno@krasava.fi>
2014-12-11 20:35:35 +0200

    prod

---
+++ common21.bash
@@ -0,0 +1,41 @@
+#alastonsuomi.info
+#bin/newsite -locale=fi_FI -themes=porno -site=alastonsuomi.info -cssthemes=simple-a
+#bin/newsite -locale=fi_FI -themes=porno -site=www.alastonsuomi.info -cssthemes=simple-a
+#bin/newsite -locale=fi_FI -themes=porno -site=blog.alastonsuomi.info  -cssthemes=simple-a
+
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=alastonsuomi.info
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=www.alastonsuomi.info
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=blog.alastonsuomi.info
+
+hugo -s www/fi_FI/porno/alastonsuomi.info
+hugo -s www/fi_FI/porno/www.alastonsuomi.info
+hugo -s www/fi_FI/porno/blog.alastonsuomi.info
+
+
+#alastonsuomi.mobi
+#bin/newsite -locale=fi_FI -themes=porno -site=alastonsuomi.mobi -cssthemes=simple-a
+#bin/newsite -locale=fi_FI -themes=porno -site=www.alastonsuomi.mobi -cssthemes=simple-a
+#bin/newsite -locale=fi_FI -themes=porno -site=blog.alastonsuomi.mobi  -cssthemes=simple-a
+
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=alastonsuomi.mobi
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=www.alastonsuomi.mobi
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=blog.alastonsuomi.mobi
+
+hugo -s www/fi_FI/porno/alastonsuomi.mobi
+hugo -s www/fi_FI/porno/www.alastonsuomi.mobi
+hugo -s www/fi_FI/porno/blog.alastonsuomi.mobi
+
+
+#alastonsuomi.org
+#bin/newsite -locale=fi_FI -themes=porno -site=alastonsuomi.org -cssthemes=simple-a
+#bin/newsite -locale=fi_FI -themes=porno -site=www.alastonsuomi.org -cssthemes=simple-a
+#bin/newsite -locale=fi_FI -themes=porno -site=blog.alastonsuomi.org  -cssthemes=simple-a
+
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=alastonsuomi.org
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=www.alastonsuomi.org
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=blog.alastonsuomi.org
+
+hugo -s www/fi_FI/porno/alastonsuomi.org
+hugo -s www/fi_FI/porno/www.alastonsuomi.org
+hugo -s www/fi_FI/porno/blog.alastonsuomi.org
+
