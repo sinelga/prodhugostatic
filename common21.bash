b30bffae07b95a8f2657aab809812898d2bf4588
juno <juno@krasava.fi>
2014-12-12 07:37:46 +0200

    prod

--- common21.bash
+++ common21.bash
@@ -39,3 +39,64 @@ hugo -s www/fi_FI/porno/alastonsuomi.org
 hugo -s www/fi_FI/porno/www.alastonsuomi.org
 hugo -s www/fi_FI/porno/blog.alastonsuomi.org
 
+#naintia.com
+#bin/newsite -locale=fi_FI -themes=porno -site=naintia.com -cssthemes=hugoscroll
+#bin/newsite -locale=fi_FI -themes=porno -site=www.naintia.com -cssthemes=hugoscroll
+#bin/newsite -locale=fi_FI -themes=porno -site=blog.naintia.com  -cssthemes=hugoscroll
+
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=naintia.com
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=www.naintia.com
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=blog.naintia.com
+
+hugo -s www/fi_FI/porno/naintia.com
+hugo -s www/fi_FI/porno/www.naintia.com
+hugo -s www/fi_FI/porno/blog.naintia.com
+
+
+#naintia.net
+#bin/newsite -locale=fi_FI -themes=porno -site=naintia.net -cssthemes=hugoscroll
+#bin/newsite -locale=fi_FI -themes=porno -site=www.naintia.net -cssthemes=hugoscroll
+#bin/newsite -locale=fi_FI -themes=porno -site=blog.naintia.net  -cssthemes=hugoscroll
+
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=naintia.net
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=www.naintia.net
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=blog.naintia.net
+
+hugo -s www/fi_FI/porno/naintia.net
+hugo -s www/fi_FI/porno/www.naintia.net
+hugo -s www/fi_FI/porno/blog.naintia.net
+
+
+#naintia.info
+#bin/newsite -locale=fi_FI -themes=porno -site=naintia.info -cssthemes=hugoscroll
+#bin/newsite -locale=fi_FI -themes=porno -site=www.naintia.info -cssthemes=hugoscroll
+#bin/newsite -locale=fi_FI -themes=porno -site=blog.naintia.info  -cssthemes=hugoscroll
+
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=naintia.info
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=www.naintia.info
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=blog.naintia.info
+
+hugo -s www/fi_FI/porno/naintia.info
+hugo -s www/fi_FI/porno/www.naintia.info
+hugo -s www/fi_FI/porno/blog.naintia.info
+
+
+#naintia.org
+#bin/newsite -locale=fi_FI -themes=porno -site=naintia.org -cssthemes=hugoscroll
+#bin/newsite -locale=fi_FI -themes=porno -site=www.naintia.org -cssthemes=hugoscroll
+#bin/newsite -locale=fi_FI -themes=porno -site=blog.naintia.org  -cssthemes=hugoscroll
+
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=naintia.org
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=www.naintia.org
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=blog.naintia.org
+
+hugo -s www/fi_FI/porno/naintia.org
+hugo -s www/fi_FI/porno/www.naintia.org
+hugo -s www/fi_FI/porno/blog.naintia.org
+
+
+
+
+
+
+
