103c360f2b8badc47ba2fcff0557f1fa1838e95d
juno <juno@krasava.fi>
2014-12-16 04:06:21 +0200

    prod

--- common22.bash
+++ common22.bash
@@ -1,4 +1,4 @@
-#suomiporno.xyz OK
+#suomiporno.xyz
 #bin/newsite -locale=fi_FI -themes=porno -site=suomiporno.xyz -cssthemes=stou-dk-theme
 #bin/newsite -locale=fi_FI -themes=porno -site=www.suomiporno.xyz -cssthemes=stou-dk-theme
 #bin/newsite -locale=fi_FI -themes=porno -site=blog.suomiporno.xyz  -cssthemes=stou-dk-theme
@@ -13,3 +13,57 @@ hugo -s www/fi_FI/porno/blog.suomiporno.xyz
 bin/sitemapsping -locale=fi_FI -themes=porno -site=suomiporno.xyz
 bin/sitemapsping -locale=fi_FI -themes=porno -site=www.suomiporno.xyz
 bin/sitemapsping -locale=fi_FI -themes=porno -site=blog.suomiporno.xyz
+
+
+#seksivideot.co
+#bin/newsite -locale=fi_FI -themes=porno -site=seksivideot.co -cssthemes=stou-dk-theme
+#bin/newsite -locale=fi_FI -themes=porno -site=www.seksivideot.co -cssthemes=stou-dk-theme
+#bin/newsite -locale=fi_FI -themes=porno -site=blog.seksivideot.co  -cssthemes=stou-dk-theme
+
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=seksivideot.co
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=www.seksivideot.co
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=blog.seksivideot.co
+
+hugo -s www/fi_FI/porno/seksivideot.co
+hugo -s www/fi_FI/porno/www.seksivideot.co
+hugo -s www/fi_FI/porno/blog.seksivideot.co
+bin/sitemapsping -locale=fi_FI -themes=porno -site=seksivideot.co
+bin/sitemapsping -locale=fi_FI -themes=porno -site=www.seksivideot.co
+bin/sitemapsping -locale=fi_FI -themes=porno -site=blog.seksivideot.co
+
+
+#seksivideot.club
+#bin/newsite -locale=fi_FI -themes=porno -site=seksivideot.club -cssthemes=stou-dk-theme
+#bin/newsite -locale=fi_FI -themes=porno -site=www.seksivideot.club -cssthemes=stou-dk-theme
+#bin/newsite -locale=fi_FI -themes=porno -site=blog.seksivideot.club  -cssthemes=stou-dk-theme
+
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=seksivideot.club
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=www.seksivideot.club
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=blog.seksivideot.club
+
+hugo -s www/fi_FI/porno/seksivideot.club
+hugo -s www/fi_FI/porno/www.seksivideot.club
+hugo -s www/fi_FI/porno/blog.seksivideot.club
+bin/sitemapsping -locale=fi_FI -themes=porno -site=seksivideot.club
+bin/sitemapsping -locale=fi_FI -themes=porno -site=www.seksivideot.club
+bin/sitemapsping -locale=fi_FI -themes=porno -site=blog.seksivideot.club
+
+
+#seksivideot.org
+#bin/newsite -locale=fi_FI -themes=porno -site=seksivideot.org -cssthemes=stou-dk-theme
+#bin/newsite -locale=fi_FI -themes=porno -site=www.seksivideot.org -cssthemes=stou-dk-theme
+#bin/newsite -locale=fi_FI -themes=porno -site=blog.seksivideot.org  -cssthemes=stou-dk-theme
+
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=seksivideot.org
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=www.seksivideot.org
+bin/jsontomarkdown -locale=fi_FI -themes=porno -site=blog.seksivideot.org
+
+hugo -s www/fi_FI/porno/seksivideot.org
+hugo -s www/fi_FI/porno/www.seksivideot.org
+hugo -s www/fi_FI/porno/blog.seksivideot.org
+bin/sitemapsping -locale=fi_FI -themes=porno -site=seksivideot.org
+bin/sitemapsping -locale=fi_FI -themes=porno -site=www.seksivideot.org
+bin/sitemapsping -locale=fi_FI -themes=porno -site=blog.seksivideot.org
+
+
+
