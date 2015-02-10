ae3dc02a8dd327745d4ebfb262ac798a8b6bc64e
juno <juno@krasava.fi>
2015-02-10 06:51:26 +0200

    dev

--- common32hugo.bash
+++ common32hugo.bash
@@ -5,4 +5,6 @@ cd /home/juno/git/prodhugostatic
 
 
 #kotirouvat.com
-bin/newsite -locale=fi_FI -themes=porno -site=www.kotirouvat.com -cssthemes=sshq
+#bin/newsite -locale=fi_FI -themes=porno -site=www.kotirouvat.com -cssthemes=sshq
+
+hugo -s www/fi_FI/porno/www.kotirouvat.com
